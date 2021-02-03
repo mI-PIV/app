d89c43e090f5593ad5056c36d123c071ae31e675
Kristoffer Price <kristoffer.k.price@gmail.com>
2021-02-02 22:33:22 -0700

    Added default window size and overlap, overlap dynamically changes to half of window size

---
+++ app/src/main/java/com/onrpiv/uploadmedia/Utilities/BoolIntStructure.java
@@ -0,0 +1,28 @@
+package com.onrpiv.uploadmedia.Utilities;
+
+
+public class BoolIntStructure {
+    private int myInt;
+    private boolean myBool;
+
+    public BoolIntStructure(boolean b, int i) {
+        myBool = b;
+        myInt = i;
+    }
+
+    public int getInt() {
+        return myInt;
+    }
+
+    public void setInt(int i) {
+        this.myInt = i;
+    }
+
+    public boolean getBool() {
+        return myBool;
+    }
+
+    public void setBool(boolean myBool) {
+        this.myBool = myBool;
+    }
+}
