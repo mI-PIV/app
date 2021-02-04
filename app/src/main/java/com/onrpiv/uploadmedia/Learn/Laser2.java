78db973a63433c9405e646818f7669fb3bc42b8d
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2021-02-04 09:31:34 -0700

    Renamed variables in all of the laserSafety .xml and .java files. They're now easier to see and understand.

--- app/src/main/java/com/onrpiv/uploadmedia/Learn/Laser2.java
+++ app/src/main/java/com/onrpiv/uploadmedia/Learn/Laser2.java
@@ -25,20 +25,20 @@ public class Laser2 extends LaserSafetyDummy {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laser2);
 
-        TextView t0 = (TextView)findViewById(R.id.laserSafetyTextView0);
+        TextView t0 = (TextView)findViewById(R.id.laserSafety2TextView0);
         t0.setText("Lasers in PIV");
         t0.setTextSize(headerTextSize);
 
-        TextView t1 = (TextView)findViewById(R.id.laserSafetyTextView2);
+        TextView t1 = (TextView)findViewById(R.id.laserSafety2TextView1);
         String html1 = "Lasers are used in PIV to illuminate neutrally buoyant particles within a flow field. These particles are typically made of silica (glass) and are designed to scatter light so cameras can capture their position accurately. Check out the Learn About PIV page to learn more.";
         t1.setText(Html.fromHtml(html1));
         t1.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
 
-        TextView t2 = (TextView)findViewById(R.id.laserSafetyTextView3);
+        TextView t2 = (TextView)findViewById(R.id.laserSafety2TextView2);
         t2.setText("To illuminate the particles, the laser passes through a cylindrical or half-circle lens (like a glass stir stick) that flattens the light into a plane.");
         t2.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
 
-        TextView t3 = (TextView)findViewById(R.id.laserSafetyTextView4);
+        TextView t3 = (TextView)findViewById(R.id.laserSafety2TextView3);
         String html2 = "You can only capture data on particles within the laser light sheet. It is safe to observe the particles moving within the plane when the laser is a sheet. They should look something like this:";
         t3.setText(Html.fromHtml(html2));
         t3.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
@@ -48,7 +48,7 @@ public class Laser2 extends LaserSafetyDummy {
             textViews[i].setTextSize(paraTextSize);
         }
 
-        VideoView videoView = (VideoView)findViewById(R.id.videoView1);
+        VideoView videoView = (VideoView)findViewById(R.id.laserSafety2VideoView0);
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.laser_safety_video);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
