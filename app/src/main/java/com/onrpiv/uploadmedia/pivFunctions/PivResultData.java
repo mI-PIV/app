a0042533ab4cf51abf3f92863403821a56918d48
Kristoffer Price <kristoffer.k.price@gmail.com>
2021-05-26 10:20:51 -0600

    Removed hashmaps from PivResultData structure and made it more generic

--- app/src/main/java/com/onrpiv/uploadmedia/pivFunctions/PivResultData.java
+++ app/src/main/java/com/onrpiv/uploadmedia/pivFunctions/PivResultData.java
@@ -1,15 +1,18 @@
 package com.onrpiv.uploadmedia.pivFunctions;
 
-import java.util.Map;
-
 public class PivResultData {
-    private Map<String, double[][]> pivCorrelation;
-    private Map<String, double[]> interrCenters;
+    private double[][] _u;
+    private double[][] _v;
+    private double[][] _mag;
+    private double[][] _sig2noise;
+
+//    private Map<String, double[][]> pivCorrelation;
+//    private Map<String, double[]> interrCenters;
     private double[][] vorticityValues;
-    private Map<String, double[][]> pivCorrelationProcessed;
-    private Map<String, double[][]> pivReplaceMissing;
-    private Map<String, double[][]> pivCorrelationMulti;
-    private Map<String, double[][]> pivReplaceMissing2;
+//    private Map<String, double[][]> pivCorrelationProcessed;
+//    private Map<String, double[][]> pivReplaceMissing;
+//    private Map<String, double[][]> pivCorrelationMulti;
+//    private Map<String, double[][]> pivReplaceMissing2;
     private int rows;
     private int cols;
 
@@ -25,62 +28,93 @@ public class PivResultData {
             COLS = "cols",
             USERNAME = "username";
 
-
-    public Map<String, double[][]> getPivCorrelation() {
-        return pivCorrelation;
+    public double[][] getMag() {
+        return _mag;
     }
 
-    public void setPivCorrelation(Map<String, double[][]> pivCorrelation) {
-        this.pivCorrelation = pivCorrelation;
+    public void setMag(double[][] magnitude) {
+        _mag = magnitude;
     }
 
-    public Map<String, double[]> getInterrCenters() {
-        return interrCenters;
+    public double[][] getSig2Noise() {
+        return _sig2noise;
     }
 
-    public void setInterrCenters(Map<String, double[]> interrCenters) {
-        this.interrCenters = interrCenters;
+    public void setSig2Noise(double[][] sig2Noise) {
+        _sig2noise = sig2Noise;
     }
 
-    public double[][] getVorticityValues() {
-        return vorticityValues;
+    public double[][] getU() {
+        return _u;
     }
 
-    public void setVorticityValues(double[][] vorticityValues) {
-        this.vorticityValues = vorticityValues;
+    public void setU(double[][] u) {
+        _u = u;
     }
 
-    public Map<String, double[][]> getPivCorrelationProcessed() {
-        return pivCorrelationProcessed;
+    public double[][] getV() {
+        return _v;
     }
 
-    public void setPivCorrelationProcessed(Map<String, double[][]> pivCorrelationProcessed) {
-        this.pivCorrelationProcessed = pivCorrelationProcessed;
+    public void setV(double[][] v) {
+        _v = v;
     }
 
-    public Map<String, double[][]> getPivReplaceMissing() {
-        return pivReplaceMissing;
-    }
-
-    public void setPivReplaceMissing(Map<String, double[][]> pivReplaceMissing) {
-        this.pivReplaceMissing = pivReplaceMissing;
-    }
-
-    public Map<String, double[][]> getPivCorrelationMulti() {
-        return pivCorrelationMulti;
-    }
-
-    public void setPivCorrelationMulti(Map<String, double[][]> pivCorrelationMulti) {
-        this.pivCorrelationMulti = pivCorrelationMulti;
-    }
-
-    public Map<String, double[][]> getPivReplaceMissing2() {
-        return pivReplaceMissing2;
-    }
-
-    public void setPivReplaceMissing2(Map<String, double[][]> pivReplaceMissing2) {
-        this.pivReplaceMissing2 = pivReplaceMissing2;
-    }
+    //    public Map<String, double[][]> getPivCorrelation() {
+//        return pivCorrelation;
+//    }
+//
+//    public void setPivCorrelation(Map<String, double[][]> pivCorrelation) {
+//        this.pivCorrelation = pivCorrelation;
+//    }
+//
+//    public Map<String, double[]> getInterrCenters() {
+//        return interrCenters;
+//    }
+//
+//    public void setInterrCenters(Map<String, double[]> interrCenters) {
+//        this.interrCenters = interrCenters;
+//    }
+//
+//    public double[][] getVorticityValues() {
+//        return vorticityValues;
+//    }
+//
+//    public void setVorticityValues(double[][] vorticityValues) {
+//        this.vorticityValues = vorticityValues;
+//    }
+//
+//    public Map<String, double[][]> getPivCorrelationProcessed() {
+//        return pivCorrelationProcessed;
+//    }
+//
+//    public void setPivCorrelationProcessed(Map<String, double[][]> pivCorrelationProcessed) {
+//        this.pivCorrelationProcessed = pivCorrelationProcessed;
+//    }
+//
+//    public Map<String, double[][]> getPivReplaceMissing() {
+//        return pivReplaceMissing;
+//    }
+//
+//    public void setPivReplaceMissing(Map<String, double[][]> pivReplaceMissing) {
+//        this.pivReplaceMissing = pivReplaceMissing;
+//    }
+//
+//    public Map<String, double[][]> getPivCorrelationMulti() {
+//        return pivCorrelationMulti;
+//    }
+//
+//    public void setPivCorrelationMulti(Map<String, double[][]> pivCorrelationMulti) {
+//        this.pivCorrelationMulti = pivCorrelationMulti;
+//    }
+//
+//    public Map<String, double[][]> getPivReplaceMissing2() {
+//        return pivReplaceMissing2;
+//    }
+//
+//    public void setPivReplaceMissing2(Map<String, double[][]> pivReplaceMissing2) {
+//        this.pivReplaceMissing2 = pivReplaceMissing2;
+//    }
 
     public int getRows() {
         return rows;
