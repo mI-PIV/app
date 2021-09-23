95b458cac379f770e2624fb6b8b5ec68f8d8ec96
Kristoffer Price <kristoffer.k.price@gmail.com>
2021-09-23 14:06:39 -0600

    Added a double frame background subtraction method to our BackgroundSub class.

--- app/src/main/java/com/onrpiv/uploadmedia/Utilities/BackgroundSub.java
+++ app/src/main/java/com/onrpiv/uploadmedia/Utilities/BackgroundSub.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.video.BackgroundSubtractor;
 import org.opencv.video.Video;
 
@@ -15,26 +17,60 @@ import java.util.Arrays;
 
 public class BackgroundSub {
     private final BackgroundSubtractor backSub;
-    private final Mat fgMask;
 
     public BackgroundSub() {
         OpenCVLoader.initDebug();
         backSub = Video.createBackgroundSubtractorMOG2();
-        fgMask = new Mat();
     }
 
     public void applyFrame(Mat frame) {
-        backSub.apply(frame, fgMask);
-    }
-
-    public Mat getFgMask() {
-        return fgMask;
+        Mat fg = new Mat();
+        backSub.apply(frame, fg);
+        fg.release();
     }
 
     public Mat getBackground() {
         Mat background = new Mat();
         backSub.getBackgroundImage(background);
         return background;
+    }
+
+    public static Mat[] doubleFrameSubtraction(Mat frame1, Mat frame2) {
+        // M. Honkanen, H. Nobach; "Background extraction from double-frame PIV images"; 2005
+
+        // convert to gray-scale
+        Mat frame1Gray = new Mat(), frame2Gray = new Mat();
+        Imgproc.cvtColor(frame1, frame1Gray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(frame2, frame2Gray, Imgproc.COLOR_BGR2GRAY);
+
+        // subtract
+        Mat diffMat = new Mat();
+        Core.subtract(frame1Gray, frame2Gray, diffMat);
+
+        // all positive values go to frame1 and all negative go to frame2
+        Mat frame1New = new Mat(), frame2New = new Mat();
+        for (int row = 0; row < diffMat.rows(); row++) {
+            for (int col = 0; col < diffMat.cols(); col++) {
+                double intensity = diffMat.get(row, col)[0];
+                if (intensity > 0) {
+                    frame1New.put(row, col, intensity);
+                } else {
+                    frame2New.put(row, col, intensity);
+                }
+            }
+        }
+
+        // change all the negative intensity values in frame2 to positive
+        Mat frame2NewPos = new Mat();
+        Core.multiply(frame2New, new Scalar(-1d), frame2NewPos);
+
+        // cleanup mats created in the method
+        frame1Gray.release();
+        frame2Gray.release();
+        diffMat.release();
+        frame2New.release();
+
+        return new Mat[] {frame1New, frame2NewPos};
     }
 
     public void subtractBackground(File framesDir) {
