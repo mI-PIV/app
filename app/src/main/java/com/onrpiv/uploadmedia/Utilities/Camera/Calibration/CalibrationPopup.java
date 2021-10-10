6bc0f08e8b970297d59bfc21963a8befc04bc1a3
Kristoffer Price <kristoffer.k.price@gmail.com>
2021-10-09 22:30:47 -0600

    Made CalibrationPopup static

--- app/src/main/java/com/onrpiv/uploadmedia/Utilities/Camera/Calibration/CalibrationPopup.java
+++ app/src/main/java/com/onrpiv/uploadmedia/Utilities/Camera/Calibration/CalibrationPopup.java
@@ -6,8 +6,6 @@ import android.net.Uri;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.ActivityResultRegistry;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 
 import com.onrpiv.uploadmedia.Utilities.PathUtil;
@@ -27,84 +25,68 @@ import java.util.Locale;
 
 
 public class CalibrationPopup {
-    private final Context context;
-    private final ActivityResultLauncher<String> cameraLauncher;
-    private boolean tryAgain = false;
-
-    public CalibrationPopup(Context context, String userName, ActivityResultRegistry resultRegistry) {
-        this.context = context;
-        cameraLauncher = resultRegistry.register("calibrationImage",
-                new ActivityResultContracts.GetContent(),
-                new ActivityResultCallback<Uri>() {
-                    @Override
-                    public void onActivityResult(Uri result) {
-                        String imagePath = PathUtil.getRealPath(context, result);
-
-                        // calculate calibration
-                        CameraCalibrationResult ccResult = new CameraCalibrationResult();
-
-                        ccResult.ratio = CameraCalibration.calculatePixelToPhysicalRatio(imagePath);
-
-                        // check to see if calibration pattern was found
-                        if (ccResult.ratio == -1d) {
-                            new AlertDialog.Builder(context)
-                                    .setMessage("Couldn't find a calibration pattern. Try again with a new picture?")
-                                    .setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            tryAgain = true;
-                                        }
-                                    })
-                                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            tryAgain = false;
-                                        }
-                                    })
-                                    .create().show();
-                            return;
-                        }
-
-                        ccResult.cameraMatrix = new Mat();
-                        ccResult.distanceCoefficients = new MatOfDouble();
-                        Mat.eye(3, 3, CvType.CV_64FC1).copyTo(ccResult.cameraMatrix);
-                        Mat.zeros(5, 1, CvType.CV_64FC1).copyTo(ccResult.distanceCoefficients);
-                        CameraCalibration.saveCameraProperties(context, ccResult.cameraMatrix, ccResult.distanceCoefficients);
-
-                        // save calibration
-                        int newCalibrationNumber = PersistedData.getTotalCalibrations(context, userName) + 1;
-                        File calibrationDir = PathUtil.getCameraCalibrationDirectory(context, userName);
-                        Date date = Calendar.getInstance().getTime();
-                        SimpleDateFormat df = new SimpleDateFormat("ddMMMyyyy", Locale.getDefault());
-
-                        try {
-                            FileOutputStream fout = new FileOutputStream(new File(calibrationDir,
-                                    "Calibration." + newCalibrationNumber + "." + df.format(date) + ".obj"));
-                            ObjectOutputStream oos = new ObjectOutputStream(fout);
-                            oos.writeObject(ccResult);
-                            fout.close();
-                            oos.close();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        // save the new calibration number
-                        PersistedData.setTotalCalibrations(context, userName, newCalibrationNumber);
-                    }
-                });
-    }
-
-    public void show() {
+    public static void show(Context context, ActivityResultLauncher<Uri> launcher) {
         new AlertDialog.Builder(context)
             .setMessage("Camera Calibration")
             .setNeutralButton("Take Calibration Picture", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    do {
-                        cameraLauncher.launch("image/*");
-                    } while(tryAgain);
+                    launcher.launch(PathUtil.getTempFileUri(context));
                 }
             })
             .setNegativeButton("Cancel", null).create().show();
+    }
+
+    public static ActivityResultCallback<Boolean> getResultCallback(Context context, String userName) {
+        return new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                String imagePath = PathUtil.getTempFilePath(context);
+
+                // calculate calibration
+                CameraCalibrationResult ccResult = new CameraCalibrationResult();
+
+                ccResult.ratio = CameraCalibration.calculatePixelToPhysicalRatio(imagePath);
+
+                // check to see if calibration pattern was found
+                if (ccResult.ratio == -1d) {
+                    new AlertDialog.Builder(context)
+                            .setMessage("Couldn't find a calibration pattern. Please make sure " +
+                                    "the calibration pattern is in the top-right quadrant of " +
+                                    "the photo and try again.")
+                            .setPositiveButton("Okay", null)
+                            .create().show();
+                } else {
+                    ccResult.cameraMatrix = new Mat();
+                    ccResult.distanceCoefficients = new MatOfDouble();
+                    Mat.eye(3, 3, CvType.CV_64FC1).copyTo(ccResult.cameraMatrix);
+                    Mat.zeros(5, 1, CvType.CV_64FC1).copyTo(ccResult.distanceCoefficients);
+                    CameraCalibration.saveCameraProperties(context, ccResult.cameraMatrix, ccResult.distanceCoefficients);
+
+                    // save calibration
+                    int newCalibrationNumber = PersistedData.getTotalCalibrations(context, userName) + 1;
+                    File calibrationDir = PathUtil.getCameraCalibrationDirectory(context, userName);
+                    Date date = Calendar.getInstance().getTime();
+                    SimpleDateFormat df = new SimpleDateFormat("ddMMMyyyy", Locale.getDefault());
+
+                    try {
+                        FileOutputStream fout = new FileOutputStream(new File(calibrationDir,
+                                "Calibration." + newCalibrationNumber + "." + df.format(date) + ".obj"));
+                        ObjectOutputStream oos = new ObjectOutputStream(fout);
+                        oos.writeObject(ccResult);
+                        fout.close();
+                        oos.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    // save the new calibration number
+                    PersistedData.setTotalCalibrations(context, userName, newCalibrationNumber);
+                }
+
+                // delete temp calibration image
+                PathUtil.deleteIfTempFile(context, imagePath);
+            }
+        };
     }
 }
