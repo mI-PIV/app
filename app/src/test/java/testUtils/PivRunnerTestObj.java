65d77a20f02753d4c81703dbc17c9d0ca7aec2ce
Kris Price <kristoffer.k.price@gmail.com>
2021-12-01 11:51:30 -0700

    Added PivRunner test object skeleton

---
+++ app/src/test/java/testUtils/PivRunnerTestObj.java
@@ -0,0 +1,24 @@
+package testUtils;
+
+import com.onrpiv.uploadmedia.pivFunctions.PivResultData;
+
+import java.util.List;
+
+public class PivRunnerTestObj {
+
+    public static List<PivResultData> runFullPIV() {
+        return null;
+    }
+
+    public static PivResultData runSinglePass() {
+        return null;
+    }
+
+    public static PivResultData runMultiPass(PivResultData singlePass) {
+        return null;
+    }
+
+    public static PivResultData runReplacement(PivResultData multiPass) {
+        return null;
+    }
+}
