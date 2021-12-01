f7fe6ff508e606ec1a210d3ffaaa0d6c155412b5
kris <kristoffer.k.price@gmail.com>
2021-12-01 15:44:59 -0700

    completed piv runner tester obj

--- app/src/test/java/testUtils/PivRunnerTestObj.java
+++ app/src/test/java/testUtils/PivRunnerTestObj.java
@@ -1,24 +1,70 @@
 package testUtils;
 
-import com.onrpiv.uploadmedia.pivFunctions.PivResultData;
+import android.test.mock.MockContext;
 
+import com.onrpiv.uploadmedia.pivFunctions.PivFunctions;
+import com.onrpiv.uploadmedia.pivFunctions.PivParameters;
+import com.onrpiv.uploadmedia.pivFunctions.PivResultData;
+import com.onrpiv.uploadmedia.pivFunctions.PivRunner;
+
+import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 public class PivRunnerTestObj {
+    private final PivParameters _params;
+    private final PivFunctions _piv;
+    private final File _frame1;
+    private final File _frame2;
+    private final MockContext _context = new MockContext();
 
-    public static List<PivResultData> runFullPIV() {
-        return null;
+
+    public PivRunnerTestObj(PivParameters params, File frame1, File frame2) {
+        _params = params;
+        _frame1 = frame1;
+        _frame2 = frame2;
+        _piv = new PivFunctions(
+                _frame1.getAbsolutePath(),
+                _frame2.getAbsolutePath(),
+                null,
+                _params,
+                null,
+                null,
+                null
+                );
     }
 
-    public static PivResultData runSinglePass() {
-        return null;
+    public HashMap<String, PivResultData> runFullPIV() {
+        PivRunner runner = new PivRunner(_context, null, _params, _frame1, _frame2);
+        return runner.Run();
     }
 
-    public static PivResultData runMultiPass(PivResultData singlePass) {
-        return null;
+    public PivResultData runSinglePass() {
+        // single pass
+        PivResultData singlePassResult = _piv.extendedSearchAreaPiv_update(PivResultData.SINGLE, null);
+
+        // vorticity
+        PivFunctions.calculateVorticityMap(singlePassResult);
+
+        return singlePassResult;
     }
 
-    public static PivResultData runReplacement(PivResultData multiPass) {
-        return null;
+    public PivResultData runPostProcessing(PivResultData singlePass) {
+        return _piv.vectorPostProcessing(singlePass, "PostProcessing");
+    }
+
+    public PivResultData runMultiPass_withReplace(PivResultData postProcessed) {
+        PivResultData pivReplaceMissing = _piv.replaceMissingVectors(postProcessed, null);
+        PivResultData pivCorrelationMulti = _piv.calculateMultipass(pivReplaceMissing, PivResultData.MULTI, null);
+        PivResultData pivReplaceMissing2 = _piv.replaceMissingVectors(pivCorrelationMulti, PivResultData.REPLACE2);
+        PivFunctions.calculateVorticityMap(pivReplaceMissing2);
+        _params.setMaxDisplacement(PivFunctions.checkMaxDisplacement(pivReplaceMissing2.getMag()));
+        return pivReplaceMissing2;
+    }
+
+    public PivResultData runReplacement_withoutReplace(PivResultData postProcessed) {
+        PivResultData pivCorrelationMulti = _piv.calculateMultipass(postProcessed, PivResultData.MULTI, null);
+        _params.setMaxDisplacement(PivFunctions.checkMaxDisplacement(pivCorrelationMulti.getMag()));
+        return pivCorrelationMulti;
     }
 }
