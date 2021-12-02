6a607b896fe0ecdc8a9f6cf9ffae5a79ed268fc6
kris <kristoffer.k.price@gmail.com>
2021-12-02 16:42:43 -0700

    added maximum magnitude filter

--- app/src/main/java/com/onrpiv/uploadmedia/pivFunctions/PivFunctions.java
+++ app/src/main/java/com/onrpiv/uploadmedia/pivFunctions/PivFunctions.java
@@ -746,10 +746,8 @@ public class PivFunctions {
 
                 // DONT ERASE COMMENTED LINE BELOW IN CASE WE NEED TO USE A SIMILAR LOGIC LATER
                 //if (pivCorrelation.get("magnitude")[k][l] * dt < nMaxUpper && pivCorrelation.get("sig2Noise")[k][l] > qMin && r_r < _e && r_c < _e) {
-                if (singlePassResult.getSig2Noise()[k][l] > qMin && r_r < _e && r_c < _e) {
-//                    if (r_r < _e && r_c < _e) {
-
-                        dr1_p[k][l] = singlePassResult.getV()[k][l];
+                if (singlePassResult.getSig2Noise()[k][l] > qMin && r_r < _e && r_c < _e && singlePassResult.getMag()[k][l] < windowSize*0.75) {
+                    dr1_p[k][l] = singlePassResult.getV()[k][l];
                     dc1_p[k][l] = singlePassResult.getU()[k][l];
                     mag_p[k][l] = singlePassResult.getMag()[k][l];
                 } else {
