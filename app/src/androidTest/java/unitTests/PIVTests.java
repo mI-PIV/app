6af2f5e32462fcffdda2e8afd2ab77fe6e04f74b
Kris Price <kristoffer.k.price@gmail.com>
2021-12-02 11:34:36 -0700

    added todos to testing

--- app/src/androidTest/java/unitTests/PIVTests.java
+++ app/src/androidTest/java/unitTests/PIVTests.java
@@ -30,15 +30,20 @@ public class PIVTests {
 
     @Test
     public void PIV_shear_dx5() {
-        // load frames and data
-        Mat frame1 = null;
-        Mat frame2 = null;
+        // TODO move and rename all frames to main resource dir
+
+        // load frames
+        Mat frame1;
+        Mat frame2;
         try {
             frame1 = Utils.loadResource(context, R.drawable.imagea_dx5);
             frame2 = Utils.loadResource(context, R.drawable.imageb_dx5);
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
+
+        //TODO load data
 
         // init piv
         PivRunnerTestObj piv = new PivRunnerTestObj(defaultParameters, frame1, frame2);
@@ -48,6 +53,9 @@ public class PIVTests {
 
         // compare results with data
 //        Assert.assertArrayEquals();
+
+        frame1.release();
+        frame2.release();
     }
 
     @Test
