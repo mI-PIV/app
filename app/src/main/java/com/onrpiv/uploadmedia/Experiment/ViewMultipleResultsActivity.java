e9ecb4285bbf5d15f83ea7d2fe05e2c96ffc9f5e
Kris Price <kristoffer.k.price@gmail.com>
2022-07-08 11:57:31 -0600

    added video creation

--- app/src/main/java/com/onrpiv/uploadmedia/Experiment/ViewMultipleResultsActivity.java
+++ app/src/main/java/com/onrpiv/uploadmedia/Experiment/ViewMultipleResultsActivity.java
@@ -1,33 +1,48 @@
 package com.onrpiv.uploadmedia.Experiment;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.onrpiv.uploadmedia.R;
+import com.onrpiv.uploadmedia.Utilities.PathUtil;
+import com.onrpiv.uploadmedia.Utilities.PersistedData;
+import com.onrpiv.uploadmedia.Utilities.VideoCreator;
 import com.onrpiv.uploadmedia.pivFunctions.PivResultData;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class ViewMultipleResultsActivity extends ViewResultsActivity {
 
     public static HashMap<Integer, HashMap<String, PivResultData>> data;
-    private int currentIndex;
-    TextView frameText;
+    private TextView frameText;
+    private String userName;
+    private String framesetName;
 
     protected void onCreate(Bundle savedInstanceState) {
         changeData(0);  // need to load the data before showing the results
         super.onCreate(savedInstanceState);  // show the results page
 
+        userName = savedInstanceState.getString(PivResultData.USERNAME);
+        framesetName = savedInstanceState.getString(PivResultData.FRAMESET);
+
         // add temporal seekbar to the results layout
         RelativeLayout base = findViewById(R.id.base_layout);
-        // TODO this isn't working, try adding the slider to frame viewer view
         base.addView(buildSliderLayout(R.id.img_frame, R.id.apply_layout));
 
         onIndexChange(0);
@@ -35,9 +50,42 @@ public class ViewMultipleResultsActivity extends ViewResultsActivity {
 
     @Override
     public void OnClick_SaveImage(View view) {
-        // TODO create and save video
-        // https://stackoverflow.com/questions/40315349/how-to-create-a-video-from-an-array-of-images-in-android
-        // TODO iterate through "data" and create a bitmap foreach imagestack
+        ImageButton saveImageButton = findViewById(R.id.imageSaveButton);
+        saveImageButton.setEnabled(false);
+        saveImageButton.setBackgroundColor(Color.parseColor("#576674"));
+
+        // create and save video
+        Context context = this;
+        File frameDir = new File(PersistedData.getFrameDirPath(context, userName, framesetName));
+        int fps = PersistedData.getFrameDirFPS(context, userName, framesetName);
+        String imageFilename = "mI_PIV_" + experimentNumber + "_" + imageCounter++ + ".png";
+
+        ContentResolver resolver = getContentResolver();
+        Uri imageCollection;
+
+        if (Build.VERSION.SDK_INT >= 29) {
+            imageCollection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        }
+        else {
+            imageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imageFilename);
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "video/mp4");
+        Uri videoUri = resolver.insert(imageCollection, contentValues);
+        String videoPath = PathUtil.getRealPath(context, videoUri);
+
+        VideoCreator.createAndSaveVideo(context, frameDir, videoPath, fps);
+
+        // popup showing user where to find the saved image
+        new AlertDialog.Builder(this)
+                .setPositiveButton("Okay", null)
+                .setMessage("Current experiment results view saved to your photo gallery.")
+                .create().show();
+
+        saveImageButton.setEnabled(true);
+        saveImageButton.setBackgroundColor(Color.parseColor("#243EDF"));
     }
 
     private void onIndexChange(int newIdx)
@@ -47,8 +95,7 @@ public class ViewMultipleResultsActivity extends ViewResultsActivity {
         settings.vortMapChanged = true;
         super.applyDisplay(null);
 
-        currentIndex = newIdx;
-        frameText.setText("frame: " + (newIdx+1));
+        frameText.setText(getFrameText(newIdx+1));
     }
 
     private void changeData(int newIdx)
@@ -88,7 +135,7 @@ public class ViewMultipleResultsActivity extends ViewResultsActivity {
         // text
         frameText = new TextView(context);
         frameText.setGravity(Gravity.CENTER);
-        frameText.setText("frame: " + currentIndex);
+        frameText.setText(getFrameText(1));
 
         // temporal seekbar
         SeekBar seekBar = new SeekBar(context);
@@ -124,5 +171,10 @@ public class ViewMultipleResultsActivity extends ViewResultsActivity {
                 // EMPTY
             }
         };
+    }
+
+    private static String getFrameText(int idx)
+    {
+        return "Frames: " + idx + " & " + (idx + 1);
     }
 }
