6bce0cf29565d13fd3b5009aee8a2456ac7b9109
Kris Price <kristoffer.k.price@gmail.com>
2022-07-21 11:02:28 -0600

    added result callback for save btn

--- app/src/main/java/com/onrpiv/uploadmedia/Experiment/PivOptionsActivity.java
+++ app/src/main/java/com/onrpiv/uploadmedia/Experiment/PivOptionsActivity.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -56,11 +56,12 @@ public class PivOptionsActivity extends AppCompatActivity {
     private Button cancelPIVDataButton;
     private CheckBox advancedCheckbox;
 
-    public PivParameters parameters;
     private ArrayList<View> hiddenViewList;
     private ArrayList<TextView> doubleTextViewList;
     private ArrayList<TextView> intTextViewList;
     private ArrayMap<Integer, String> idToKey;
+
+    private PivParameters parameters;
 
     public static final String
             FRAMESET = "frameSetName",
@@ -68,13 +69,14 @@ public class PivOptionsActivity extends AppCompatActivity {
             FRAMETWO = "frameTwo";
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_piv_dialog);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // get data from Image activity
         Intent thisIntent = getIntent();
         String userName = thisIntent.getStringExtra("UserName");
         String frameSetName = thisIntent.getStringExtra(FRAMESET);
@@ -181,11 +183,28 @@ public class PivOptionsActivity extends AppCompatActivity {
         setListeners(this, userName);
     }
 
-    public void setSaveListener(View.OnClickListener saveListener) {
-        savePIVDataButton.setOnClickListener(saveListener);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setListeners(Context context, String userName) {
+        // save button
+        savePIVDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("params", parameters);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
         // Hide/Show advanced options
         advancedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
